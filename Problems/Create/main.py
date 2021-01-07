4d24f1e8552efc778a5d103ca9f781c09387e38d
Sergey Goppikov <grey2k@gmail.com>
2021-01-08 01:26:53 +0300

    3/5 completed

---
+++ Problems/Create/main.py
@@ -0,0 +1,4 @@
+# create a dict here
+friends = {'Alex': 'field flowers', 'Kate': 'daffodil', 'Eva': 'artichoke flower', 'Daniel': 'tulip'}
+# and print it
+print(friends)
--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,13 +1,43 @@
+import random
+
 # Write your code here
-pick = input().strip()
+ROCK = 'rock'
+PAPER = 'paper'
+SCISSORS = 'scissors'
 
-contra = None
+COMMAND_EXIT = '!exit'
 
-if pick == 'rock':
-    contra = 'paper'
-if pick == 'paper':
-    contra = 'scissors'
-if pick == 'scissors':
-    contra = 'rock'
+combinations = {ROCK: PAPER, PAPER: SCISSORS, SCISSORS: ROCK}
 
-print(f'Sorry, but the computer chose {contra}')
+
+def is_winner(pick: str, option: str):
+    if combinations[pick] == option:
+        return False
+    if combinations[option] == pick:
+        return True
+
+    return None
+
+
+# Entrypoint
+while True:
+    input_pick = input().strip()
+
+    if input_pick not in (list(combinations.keys()) + [COMMAND_EXIT]):
+        print('Invalid input')
+        continue
+
+    if input_pick == COMMAND_EXIT:
+        break
+
+    input_option = random.choice(list(combinations.keys()))
+    result = is_winner(input_pick, input_option)
+
+    if result is True:
+        print(f'Well done. The computer chose {input_option} and failed')
+    elif result is False:
+        print(f'Sorry, but the computer chose {input_option}')
+    else:
+        print(f'There is a draw ({input_option})')
+
+print('Bye!')
